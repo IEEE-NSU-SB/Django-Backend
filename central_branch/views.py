bf442f20e09dd5252eca16c2412f6680a652333b
Arman Mokammel <armanmokammel@gmail.com>
2025-03-06 22:18:22 +0600

    Task history added to task homepage

--- central_branch/views.py
+++ central_branch/views.py
@@ -4764,6 +4764,7 @@ def task_home(request,team_primary = None):
         #getting all task categories
         all_task_categories = Task_Category.objects.all().order_by('name')
         all_branch_panels = Branch.load_all_panels()
+        branch_panel = None
 
         if request.method == "POST":
 
@@ -4804,7 +4805,15 @@ def task_home(request,team_primary = None):
         #########
         has_task_create_access = Branch_View_Access.get_create_individual_task_access(request, team_primary,permission_for_co_ordinator_and_incharges_to_create_task) or Branch_View_Access.get_create_team_task_access(request, team_primary,permission_for_co_ordinator_and_incharges_to_create_task)
         #########
-        all_tasks = Task_Assignation.load_task_for_home_page(team_primary)
+        if request.GET.get('panel') and request.GET.get('panel') != '':
+            panel = request.GET.get('panel')
+            branch_panel = Panels.objects.get(panel_of__primary=1, year=panel)
+        else:
+            branch_panel = Branch.load_current_panel()
+
+        all_tasks = Task_Assignation.load_task_for_home_page(team_primary, branch_panel)
+               
+
         if team_primary == None or team_primary == "1":
 
             
@@ -4816,7 +4825,7 @@ def task_home(request,team_primary = None):
             'all_task_categories':all_task_categories,
             'all_branch_panels':all_branch_panels,
             'has_task_create_access':has_task_create_access,
-
+            'branch_panel':branch_panel,
             'app_name':app_name,
             }
 
