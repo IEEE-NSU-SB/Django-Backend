1db6343260ea27947670befa21f03ec5bfb1ead4
Arman Mokammel <armanmokammel@gmail.com>
2025-04-10 02:42:53 +0600

    .

--- chapters_and_affinity_group/manage_access.py
+++ chapters_and_affinity_group/manage_access.py
@@ -299,10 +299,7 @@ class SC_Ag_Render_Access:
             budget_sheet = BudgetSheet.objects.filter(event=event_id)
             get_member = None
             if budget_sheet.exists():
-                if budget_sheet[0].sheet_of.primary == primary:
-                    get_member = BudgetSheetAccess.objects.filter(member=username, sheet_id=budget_sheet[0].id)
-                else:
-                    return 'Restricted'
+                get_member = BudgetSheetAccess.objects.filter(member=username, sheet_id=budget_sheet[0].id)
                 
             #Check if the member exits
             if(get_member):
