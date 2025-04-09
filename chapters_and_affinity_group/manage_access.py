3ca9b92b3a25a335761a69f0f4f9b87c8d9188dd
Arman Mokammel <armanmokammel@gmail.com>
2025-04-10 02:05:06 +0600

    Budget access sc ag fix

--- chapters_and_affinity_group/manage_access.py
+++ chapters_and_affinity_group/manage_access.py
@@ -49,6 +49,36 @@ class SC_Ag_Render_Access:
             # SC_Ag_Render_Access.logger.error("An error occurred at {datetime}".format(datetime=datetime.now()), exc_info=True)
             return False
         
+    def get_sc_ag_common_access_non_branch(request,sc_ag_primary):
+        try:
+            # get the user and username. Username will work as IEEE ID and Developer username both
+            user=request.user
+            username=user.username
+            
+            # generate superuser or staff user access
+            system_manager_access=False
+            if(Access_Render.system_administrator_superuser_access(username=username) or Access_Render.system_administrator_staffuser_access(username=username)):
+                system_manager_access=True
+            
+            #generate sc_ag eb access
+            sc_ag_eb_access=False
+            if(Access_Render.sc_ag_eb_access(username=username, sc_ag_primary=sc_ag_primary)):
+                sc_ag_eb_access=True
+            
+            # # generate Faculty Advisor Access
+            # faculty_advisor_access=False
+            # if(Access_Render.faculty_advisor_access(username=username)):
+            #     faculty_advisor_access=True
+            
+            # if any of this is true, grant access
+            if(system_manager_access or sc_ag_eb_access):
+                return True
+            else:
+                return False
+        except Exception as e:
+            # SC_Ag_Render_Access.logger.error("An error occurred at {datetime}".format(datetime=datetime.now()), exc_info=True)
+            return False
+        
     def access_for_sc_ag_updates(request):
         try:
             # get the user and username. Username will work as IEEE ID and Developer username both
@@ -275,18 +305,18 @@ class SC_Ag_Render_Access:
             if(get_member):
                 if get_member.exists():
                     #The member exists. Now check if it has budget access
-                    if(SC_Ag_Render_Access.get_sc_ag_common_access(request, primary)):
+                    if(SC_Ag_Render_Access.get_sc_ag_common_access_non_branch(request, primary)):
                         return 'Edit'
                     else:
                         return get_member[0].access_type
                 else:
                     #The member does not exist in the permissions table
-                    if(SC_Ag_Render_Access.get_sc_ag_common_access(request, primary)):
+                    if(SC_Ag_Render_Access.get_sc_ag_common_access_non_branch(request, primary)):
                         return 'Edit'
                     else:
                         return 'Restricted'
         except Exception as e:
-            if(SC_Ag_Render_Access.get_sc_ag_common_access(request, primary)):
+            if(SC_Ag_Render_Access.get_sc_ag_common_access_non_branch(request, primary)):
                 return 'Edit'
             else:
                 SC_Ag_Render_Access.logger.error("An error occurred at {datetime}".format(datetime=datetime.now()), exc_info=True)
