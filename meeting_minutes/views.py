06477422c39acbef6d54cf6a76f2fa4175a4cfc7
Fardin Khan <fardin.frm4585@gmail.com>
2025-06-20 14:19:15 +0600

    meeting_date added

--- meeting_minutes/views.py
+++ meeting_minutes/views.py
@@ -2,69 +2,83 @@ from datetime import datetime, time
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 import os
-from central_branch import renderData
+from users import renderData
 from insb_port import settings
 from meeting_minutes.models import MeetingMinutes
 from django.views.decorators.csrf import csrf_exempt
 from reportlab.lib.utils import ImageReader
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
+
+from port.renderData import PortData
 # from . import renderData
 # from meeting_minutes.renderData import team_mm_info,branch_mm_info
 
 # Create your views here.
-def meeting_minutes_homepage(request):
-    meetings = MeetingMinutes.objects.all().order_by('-date')
-    return render(request, 'meeting_minutes_homepage.html', {'meetings': meetings})
 
-def team_meeting_minutes(request):
-    '''
-    Loads all the teams' exisitng meeting minutes
-    Gives option to add or delete a meeting minutes
-    '''
+# def team_meeting_minutes(request):
+#     '''
+#     Loads all the teams' exisitng meeting minutes
+#     Gives option to add or delete a meeting minutes
+#     '''
      
-    #load teams' meeting minutes from database
+#     #load teams' meeting minutes from database
     
-    teams_mm=renderData.team_meeting_minutes.load_all_team_mm()
-    team_mm_list=[]
-    for team in teams_mm:
-        team_mm_list.append(team)
-    context={
-        'team':team_mm_list
+#     teams_mm=renderData.team_meeting_minutes.load_all_team_mm()
+#     team_mm_list=[]
+#     for team in teams_mm:
+#         team_mm_list.append(team)
+#     context={
+#         'team':team_mm_list
+#     }
+
+#     return render(request,'team_meeting_minutes.html')
+
+
+# def branch_meeting_minutes(request):
+#     '''
+#     Loads all the branchs' existing meeting minutes
+#     Gives option to add or delete a meeting minutes
+#     '''
+    
+#     #load branchs' meeting minutes from database
+    
+#     branch_mm=renderData.branch_meeting_minutes.load_all_branch_mm()
+#     branch_mm_list=[]
+#     for branch in branch_mm:
+#         branch_mm_list.append(branch)
+#     context={
+#         'team':branch_mm_list
+#     }
+#     return render(request,'branch_meeting_minutes.html')
+
+
+#     #meeting_minutes_edit
+
+
+def meeting_minutes_homepage(request, primary=None):
+    sc_ag=PortData.get_all_sc_ag(request=request)
+    current_user=renderData.LoggedinUser(request.user) #Creating an Object of logged in user with current users credentials
+    user_data=current_user.getUserData() #getting user data as dictionary file
+    get_sc_ag_info=None
+        
+    meetings = MeetingMinutes.objects.all().order_by('-meeting_date')
+    
+    context = {
+        'all_sc_ag':sc_ag,
+        'sc_ag_info':get_sc_ag_info,
+        'user_data':user_data,
+        'primary': primary,
+        'meetings': meetings,
     }
+    return render(request, 'meeting_minutes_homepage.html', context)
 
-    return render(request,'team_meeting_minutes.html')
-
-
-def branch_meeting_minutes(request):
-    '''
-    Loads all the branchs' existing meeting minutes
-    Gives option to add or delete a meeting minutes
-    '''
-    
-    #load branchs' meeting minutes from database
-    
-    branch_mm=renderData.branch_meeting_minutes.load_all_branch_mm()
-    branch_mm_list=[]
-    for branch in branch_mm:
-        branch_mm_list.append(branch)
-    context={
-        'team':branch_mm_list
-    }
-    return render(request,'branch_meeting_minutes.html')
-
-
-    #meeting_minutes_edit
-
-
-
-
-def meeting_minutes_create(request):
+def meeting_minutes_create(request, primary=None):
     if request.method == 'POST':
         location = request.POST.get('location')
         start_time_raw = request.POST.get('start_time')
         end_time_raw = request.POST.get('end_time')
-
+        meeting_date=request.POST.get('meeting_date')
         if 'T' in start_time_raw:
             dt_obj = datetime.fromisoformat(start_time_raw)
             date = dt_obj.date()
@@ -94,7 +108,7 @@ def meeting_minutes_create(request):
             location=location,
             start_time=start_time,
             end_time=end_time,
-            date=date,
+            meeting_date=meeting_date,
             venue=venue,
             total_attendee=total_attendee,
             ieee_attendee=ieee_attendee,
@@ -107,43 +121,44 @@ def meeting_minutes_create(request):
             written_by=written_by
         )
 
-        return redirect('meeting_minutes:meeting_minutes_homepage')
+        return redirect('central_branch:meeting_minutes:meeting_minutes_homepage')
 
     return render(request, 'meeting_minutes_edit.html', {'meeting': None})
 
 
 def meeting_minutes_edit(request, pk):
-    meeting = get_object_or_404(MeetingMinutes, pk=pk)
+    meeting = MeetingMinutes.objects.get(pk=pk)
 
     if request.method == 'POST':
-        location = request.POST.get('location')
-        start_time_raw = request.POST.get('start_time')
-        end_time_raw = request.POST.get('end_time')
+        if 'save' in request.POST:
+            location = request.POST.get('location')
+            start_time_raw = request.POST.get('start_time')
+            end_time_raw = request.POST.get('end_time')
 
-        start_time = datetime.fromisoformat(start_time_raw).time() if 'T' in start_time_raw else datetime.strptime(start_time_raw, '%H:%M').time()
-        end_time = datetime.fromisoformat(end_time_raw).time() if 'T' in end_time_raw else datetime.strptime(end_time_raw, '%H:%M').time()
+            start_time = datetime.fromisoformat(start_time_raw).time() if 'T' in start_time_raw else datetime.strptime(start_time_raw, '%H:%M').time()
+            end_time = datetime.fromisoformat(end_time_raw).time() if 'T' in end_time_raw else datetime.strptime(end_time_raw, '%H:%M').time()
+            meeting.meeting_date=request.POST.get('meeting_date')
+            meeting.meeting_name = request.POST.get('meeting_name')
+            meeting.location = location
+            meeting.start_time = start_time
+            meeting.end_time = end_time
+            meeting.venue = request.POST.get('venue')
+            meeting.total_attendee = int(request.POST.get('total_attendees', 0))
+            meeting.ieee_attendee = request.POST.get('ieee_attendee')
+            meeting.non_ieee_attendee = request.POST.get('non_ieee_attendee')
+            meeting.agendas = request.POST.getlist('agenda[]')
+            meeting.discussion = request.POST.get('discussion')
+            meeting.host = request.POST.get('host')
+            meeting.co_host = request.POST.get('co_host')
+            meeting.guest = request.POST.get('guest')
+            meeting.written_by = request.POST.get('written_by')
 
-        meeting.meeting_name = request.POST.get('meeting_name')
-        meeting.location = location
-        meeting.start_time = start_time
-        meeting.end_time = end_time
-        meeting.venue = request.POST.get('venue')
-        meeting.total_attendee = int(request.POST.get('total_attendees', 0))
-        meeting.ieee_attendee = request.POST.get('ieee_attendee')
-        meeting.non_ieee_attendee = request.POST.get('non_ieee_attendee')
-        meeting.agendas = request.POST.getlist('agenda[]')
-        meeting.discussion = request.POST.get('discussion')
-        meeting.host = request.POST.get('host')
-        meeting.co_host = request.POST.get('co_host')
-        meeting.guest = request.POST.get('guest')
-        meeting.written_by = request.POST.get('written_by')
-
-        if 'delete' in request.POST:
-            meeting.delete()
-        else:
             meeting.save()
+            
+        elif 'delete' in request.POST:
+            meeting.delete()
 
-        return redirect('meeting_minutes:meeting_minutes_homepage')
+        return redirect('central_branch:meeting_minutes:meeting_minutes_homepage')
 
     return render(request, 'meeting_minutes_edit.html', {'meeting': meeting})
 
