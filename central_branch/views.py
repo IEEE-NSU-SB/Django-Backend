2245b5aaf7771b07c320b8b9bb636ce3ba6f6bd8
Arman Mokammel <armanmokammel@gmail.com>
2025-09-17 02:15:43 +0600

    .

--- central_branch/views.py
+++ central_branch/views.py
@@ -1132,8 +1132,10 @@ def manage_website_homepage(request):
             
             toggle, created = MediaToggle.objects.get_or_create(id=1)
             video_banner = HomePageTopBanner.objects.filter(media_type='video')
-            if video_banner:
+            if video_banner.video:
                 video_banner = video_banner.first()
+            else:
+                video_banner = False
             image_banners = HomePageTopBanner.objects.filter(media_type='image')
             
             # Getting Form response
