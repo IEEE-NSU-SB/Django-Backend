7ced8f818726336a36540d681727bd776a4325f1
Rahiq Rahman <rahiqrahman2001@gmail.com>
2026-02-10 16:40:11 +0600

    otp page added

--- graphics_team/views.py
+++ graphics_team/views.py
@@ -405,3 +405,8 @@ def certificate_page_type(request):
 @member_login_permission
 def certificate_details_page(request):
     return render(request, "certificate/certificate_details_page.html")
+
+@login_required
+@member_login_permission
+def certificate_otp(request):
+    return render(request, "certificate/certificate_otp.html")
